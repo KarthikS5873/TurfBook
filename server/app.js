ed6d7ac1e9e1398972a6c829b96e60abc296270f
KarthikS5873 <karthikrajan5873@gmail.com>
2026-06-19 12:06:04 +0530

    chore: add hardcoded Vercel URL to CORS origins

--- server/app.js
+++ server/app.js
@@ -11,6 +11,7 @@ const app = express();
 const allowedOrigins = [
   'http://localhost:5173',
   'http://localhost:3000',
+  'https://turf-book-five.vercel.app',
   ...(process.env.CLIENT_URL ? process.env.CLIENT_URL.split(',').map(s => s.trim()) : [])
 ].filter(Boolean);
 
